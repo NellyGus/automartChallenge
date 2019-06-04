921bbf469fc0287c5d9fdf74b41c43bfb1395d9b
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-04 15:36:17 +0000

    feature (data): user represent

---
+++ server/model/user_model.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+    id: 1,
+    email: "email@gmail.com",
+    first_name: "firstname",
+    last_name: "lastname",
+    password: "password",
+    address: "address",
+    is_admin: "true"
+    }
+]
+
+export default users;
