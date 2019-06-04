261da0bb371cbc0d66bf35d53c88e21d1f12b7a1
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-04 15:34:32 +0000

    feature (data): flag represent

---
+++ server/model/flag_model.js
@@ -0,0 +1,12 @@
+
+ const flags = [
+     {
+         id: 1,
+         car_id: 1,
+         created_on: new Date(),
+         reason: "reason",
+         description: "description"
+     }
+ ]
+
+ export default flags;
