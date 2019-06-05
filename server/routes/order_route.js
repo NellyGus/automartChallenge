e0b39e2ea18f1881ccdfc1b00c72421457fa5d13
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-05 22:52:53 +0000

    feature(update-validate-order): user must provide validate prive and offered price

--- server/routes/order_route.js
+++ server/routes/order_route.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import order_controller from '../controllers/order_controller';
 import auth from '../middlewares/auth';
+import validPrice from '../middlewares/valid_price';
+import validPriceOffered from '../middlewares/valid_price_offered';
+import validNewPriceOffered from '../middlewares/valid_new_price'; 
+
 
 const order_route = express.Router();
 
-order_route.post('/api/v1/order', auth, order_controller.makeOrder),
-order_route.patch('/api/v1/:order_id/price', auth, order_controller.updatePrice)
+order_route.post('/api/v1/order', auth, validPrice, validPriceOffered, order_controller.makeOrder),
+order_route.patch('/api/v1/:order_id/price', auth, validNewPriceOffered, order_controller.updatePrice)
 
 export default order_route;
