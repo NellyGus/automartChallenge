cf13750d83626522cbfd45204aca90a5c6e5901f
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-07 21:34:52 +0000

    Update user_test.js

--- server/test/user_test.js
+++ server/test/user_test.js
@@ -2,7 +2,7 @@ import chai from 'chai';
 import { expect, assert } from 'chai';
 import chaiHttp from 'chai-http';
 import { describe, it } from 'mocha';
-import app from '../app';
+import app from '../server';
 
 chai.use(chaiHttp);
 
