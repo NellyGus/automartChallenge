1e6fbd9aea8d660de003018c2c1fadb8dc3960c8
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-12 17:46:04 +0000

    feature(specific-car): action on click on report or purchase button

--- ui/js/detail_car.js
+++ ui/js/detail_car.js
@@ -1,7 +1,23 @@
-document.getElementById('reportBtn').addEventListener('click', function(){
+document.getElementById('reportBtn').addEventListener('click', ()=>{
 	document.querySelector('.bg-modal').style.display='flex';
+});
+
+document.getElementById('purchaseBtn').addEventListener('click', ()=> {
+	document.querySelector('.po-modal').style.display='flex';
+});
+
+document.getElementById('contactBtn').addEventListener('click', ()=> {
+	document.querySelector('.contact-modal').style.display='flex';
 });
 
 document.querySelector('.close').addEventListener('click', function(){
 	document.querySelector('.bg-modal').style.display='none';
+});
+
+document.querySelector('.close2').addEventListener('click', function(){
+	document.querySelector('.po-modal').style.display='none';
+});
+
+document.querySelector('.close3').addEventListener('click', function(){
+	document.querySelector('.contact-modal').style.display='none';
 });
