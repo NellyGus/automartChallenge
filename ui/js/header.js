3a35bd64c65ebed8680e19cc2b7218e63f2f349f
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-13 09:24:31 +0000

    Update header.js

--- ui/js/header.js
+++ ui/js/header.js
@@ -1,22 +1,24 @@
-// $(".mobile_nav").click(function() {
 
-//   var mm = $(".mobile_menu"),
-//       mn = $(".mobile_nav"),
-//     a = "active";
+$(".mobile_nav").click(()=> {
+
+    let mm = $(".mobile_menu"),
+        mn = $(".mobile_nav"),
+      a = "active";
+    
+    if (mm.hasClass(a) && mn.hasClass(a)) {
+      mm.removeClass(a).fadeOut(200);
+      mn.removeClass(a);
+      $('.mobile_menu li').each(()=>{
+        $(this).removeClass('slide');
+      });
+    } else {
+      mm.addClass(a).fadeIn(200);
+      mn.addClass(a);
+      $('.mobile_menu li').each((i) =>{
+      var t = $(this);
+      setTimeout(() => { t.addClass('slide'); }, (i+1) * 100);
+    });
+    }
+    
+  });
   
-//   if (mm.hasClass(a) && mn.hasClass(a)) {
-//     mm.removeClass(a).fadeOut(200);
-//     mn.removeClass(a);
-//     $('.mobile_menu li').each(function(){
-//       $(this).removeClass('slide');
-//     });
-//   } else {
-//     mm.addClass(a).fadeIn(200);
-//     mn.addClass(a);
-//     $('.mobile_menu li').each(function(i){
-//     var t = $(this);
-//     setTimeout(function(){ t.addClass('slide'); }, (i+1) * 100);
-//   });
-//   }
-  
-// });
