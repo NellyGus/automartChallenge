b3c98dc124f9d2bdb103d3573f6764eb37947eee
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-13 12:16:54 +0000

    Create valid_email.js.map

---
+++ build/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var PORT = exports.PORT = 8080;
+var JWT_SECRET = exports.JWT_SECRET = 'secret';
+//# sourceMappingURL=config.js.map
