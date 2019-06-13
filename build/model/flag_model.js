3b129c6c3674484003abffaa47ad61b867ebb106
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-13 12:21:14 +0000

    Create flag_model.js

---
+++ build/model/flag_model.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var flags = [{
+    id: 1,
+    car_id: 1,
+    created_on: new Date(),
+    reason: "reason",
+    description: "description"
+}];
+
+exports.default = flags;
+//# sourceMappingURL=flag_model.js.map
