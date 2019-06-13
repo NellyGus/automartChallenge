f1c9a70327880681828c04d4f5876ee2941dd403
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-13 12:21:07 +0000

    Create car_model.js

---
+++ build/model/car_model.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var cars = [{
+    id: 1,
+    owner: 2,
+    created_on: new Date(),
+    state: "used",
+    status: "available",
+    price: 1200,
+    manufacturer: "Range Rover",
+    model: "5.0 LC",
+    year: 2015,
+    more_description: 'more description'
+}];
+
+exports.default = cars;
+//# sourceMappingURL=car_model.js.map
