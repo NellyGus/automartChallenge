96af9f54b0240ecbae0b983f914ea1ced98875a4
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-13 12:21:30 +0000

    Create user_model.js

---
+++ build/model/user_model.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var users = [{
+    id: 1,
+    email: "email@gmail.com",
+    first_name: "firstname",
+    last_name: "lastname",
+    password: "password",
+    address: "address",
+    is_admin: "true"
+}];
+
+exports.default = users;
+//# sourceMappingURL=user_model.js.map
