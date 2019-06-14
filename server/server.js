e7110e9f197ae4a09b6f8b88f6e4c5588a903006
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-14 12:55:25 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import log from 'fancy-log';
+import cors from 'cors';
+import morgan from 'morgan';
 import car_router from './routes/car_route';
 import flag_router from './routes/flag_route';
 import order_router from './routes/order_route';
 import user_route from './routes/user_route';
-import {PORT} from './config';
+// import {PORT} from './config';
 
 
 const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,8 +25,8 @@ app.use(user_route);
 app.use(flag_router);
 app.use(order_router);
 
-app.listen(PORT, ()=>{
-    console.debug('server running on port '+PORT);
-});
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => log.info(`Listening at ${PORT}`));
 
 export default app;
