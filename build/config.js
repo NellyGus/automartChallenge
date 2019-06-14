184458dc6b8543fd2d11a5f1822824c83c215eaf
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-14 20:02:58 +0000

    feature(url): Handle unavailable url

--- build/config.js
+++ build/config.js
@@ -3,6 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var PORT = exports.PORT = 8080;
+// export const PORT = 8080;
 var JWT_SECRET = exports.JWT_SECRET = 'secret';
 //# sourceMappingURL=config.js.map
