7df73f82e718301bb8c3de505cce1e8a31552a0f
NellyGus <39280634+NellyGus@users.noreply.github.com>
2019-06-14 20:03:56 +0000

    Update server.js

--- build/server.js
+++ build/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _express = require('express');
@@ -11,6 +11,22 @@ var _express2 = _interopRequireDefault(_express);
 var _bodyParser = require('body-parser');
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
+var _fancyLog = require('fancy-log');
+
+var _fancyLog2 = _interopRequireDefault(_fancyLog);
+
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
 
 var _car_route = require('./routes/car_route');
 
@@ -28,11 +44,17 @@ var _user_route = require('./routes/user_route');
 
 var _user_route2 = _interopRequireDefault(_user_route);
 
-var _config = require('./config');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// import {PORT} from './config';
+
+
 var app = (0, _express2.default)();
+
+_dotenv2.default.config();
+
+app.use((0, _cors2.default)());
+app.use((0, _morgan2.default)('dev'));
 
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: true }));
@@ -41,8 +63,10 @@ app.use(_user_route2.default);
 app.use(_flag_route2.default);
 app.use(_order_route2.default);
 
-app.listen(_config.PORT, function () {
-    console.debug('server running on port ' + _config.PORT);
+var PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function () {
+  return _fancyLog2.default.info('Listening at ' + PORT);
 });
 
 exports.default = app;
